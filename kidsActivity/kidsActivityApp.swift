0cd84a923e393bae0dcf9e9915d30923c9095b04
sumayah194 <saalrowaibaah22@twq.idserve.net>
2022-11-01 21:15:00 +0300

    Initial Commit

---
+++ kidsActivity/kidsActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidsActivityApp.swift
+//  kidsActivity
+//
+//  Created by sumayah on 07/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct kidsActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
